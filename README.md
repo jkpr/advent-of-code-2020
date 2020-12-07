a3b0332bec0b53fa001e015557bf6f4f34ca92f6
James <jameskpringle@gmail.com>
2020-12-06 20:38:40 -0600

    Add Day 4 code

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ These are my solutions to Advent of Code 2020.
 
 Try the problems yourself at [https://adventofcode.com/2020/](https://adventofcode.com/2020/).
 
+Feel free to create a Github issue if you want to discuss anything!
+
 # Day 1
 
 This is a warm up challenge. Python's standard library comes to the rescue with the [`itertools` library and `combinations` function][1a].
@@ -21,3 +23,13 @@ At this point, Python's `itertools` and `collections` modules have been useful, 
 # Day 3
 
 The main thing to realize is that since the forest is repeating along the x-axis, we can use module arithmetic on the position of the x-axis index.
+
+# Day 4
+
+Some useful things to note.
+
+1. I went with the [`re` module][4a] because the text followed patterns seemingly perfectly.
+2. There is a difference in Python between `re.search` (find regex anywhere in the string), `re.match` (find regex starting at the beginning of the string) and `re.fullmatch` (the regex must match the full string). I first did this with `re.match` and a regex like `\d{4}` could match something like `20202`. Of course, there are also the meta characters `^` and `$` to denote the start and end of a line.
+3.  The `re` match object has a `.groups()` method which returns just the pieces that match the regex in parentheses `()`. There is also a `.group(n)` method that is similar. `.group(0)` returns the full match, then `1` up to `n` match the pieces matched in parentheses.
+
+[4a]: https://docs.python.org/3/library/re.html
---
+++ advent/day4/__init__.py
@@ -0,0 +1,76 @@
+from collections import Counter
+import re
+from typing import List
+
+
+def part1(lines: List[str]):
+    count = 0
+    joined = '\n'.join(lines)
+    split = joined.split('\n\n')
+    for passport in split:
+        passport_split = passport.split()
+        pp_dict = dict(re.fullmatch(r"(\w{3}):(\S+)", item).groups() for item in passport_split)
+        size_valid = len(pp_dict) == 8
+        optional_cid = len(pp_dict) == 7 and 'cid' not in pp_dict
+        if size_valid or optional_cid:
+            count += 1
+    print(count)
+
+
+# --- --- # --- --- # --- --- # --- --- # --- --- 
+# The following is for part 2. It has some duplicate code from part 1
+# --- --- # --- --- # --- --- # --- --- # --- --- 
+
+
+def get_passports(lines: List[str]):
+    result = []
+    joined = '\n'.join(lines)
+    split = joined.split('\n\n')
+    for passport in split:
+        passport_split = passport.split()
+        pp_dict = dict(re.fullmatch(r"(\w{3}):(\S+)", item).groups() for item in passport_split)
+        result.append(pp_dict)
+    return result
+
+
+def is_valid_pp(pp: dict):
+    if len(pp) <= 6:
+        return False
+    if len(pp) == 7 and 'cid' in pp:
+        return False
+    try:
+        byr = int(re.fullmatch(r"\d{4}", pp["byr"]).group(0))
+        if not 1920 <= byr <= 2002:
+            return False
+        iyr = int(re.fullmatch(r"\d{4}", pp["iyr"]).group(0))
+        if not 2010 <= iyr <= 2020:
+            return False
+        eyr = int(re.fullmatch(r"\d{4}", pp["eyr"]).group(0))
+        if not 2020 <= eyr <= 2030:
+            return False
+        hgt = re.fullmatch(r"(\d+)(cm|in)", pp["hgt"])
+        if hgt.group(2) == 'in' and not 59 <= int(hgt.group(1)) <= 76:
+            return False
+        if hgt.group(2) == 'cm' and not 150 <= int(hgt.group(1)) <= 193:
+            return False
+    except AttributeError:
+        return False
+    hcl = re.fullmatch(r"#[0-9a-f]{6}", pp["hcl"])
+    if not hcl:
+        return False
+    ecl = re.fullmatch(r"(amb|blu|brn|gry|grn|hzl|oth)", pp["ecl"])
+    if not ecl:
+        return False
+    pid = re.fullmatch(r"\d{9}", pp["pid"])
+    if not pid:
+        return False
+    return True
+
+
+def part2(lines: List[int]):
+    count = 0
+    pp_list = get_passports(lines)
+    for pp in pp_list:
+        if is_valid_pp(pp):
+            count += 1
+    print(count)
